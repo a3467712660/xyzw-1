1ef174e4ca4446522669536765251aa12f54b961
wingera <wingera@users.noreply.github.com>
2026-04-19 14:52:19 +0800

    Bump Android version and replace download APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -77,8 +77,8 @@ android {
     applicationId = "com.xyzw.helper"
     minSdk = 26
     targetSdk = 35
-    versionCode = 6
-    versionName = "1.0.5"
+    versionCode = 7
+    versionName = "1.0.6"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables.useSupportLibrary = true
   }
